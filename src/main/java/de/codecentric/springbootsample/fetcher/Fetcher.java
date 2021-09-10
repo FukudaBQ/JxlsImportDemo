b78d7bc7516b2ca00a9858accff615a502bc1078
fukudaBQ <boweirocks@gmail.com>
2021-09-10 17:48:06 +0800

    wait for test, save purpose

--- src/main/java/de/codecentric/springbootsample/fetcher/Fetcher.java
+++ src/main/java/de/codecentric/springbootsample/fetcher/Fetcher.java
@@ -1,8 +1,27 @@
 package de.codecentric.springbootsample.fetcher;
 
+import de.codecentric.springbootsample.Dao.App;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.jxls.reader.ReaderBuilder;
+import org.jxls.reader.XLSReadStatus;
+import org.jxls.reader.XLSReader;
+import org.xml.sax.SAXException;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fetcher implements IFetcher{
     @Override
-    public boolean fetchExcel() {
+    public boolean fetchExcel() throws IOException, SAXException, InvalidFormatException {
+        File file;
+        InputStream inputXML = new FileInputStream("D:\\project\\JXLS Import\\springboot-sample-app-master\\JxlsImportDemo\\src\\main\\resources\\templates\\inputConfig.xml");
+        XLSReader reader = ReaderBuilder.buildFromXML(inputXML);
+        InputStream inputXLS = new FileInputStream("D:\\project\\JXLS Import\\springboot-sample-app-master\\JxlsImportDemo\\src\\main\\resources\\xlsFiles");
+        App app = new App();
+        Map beans = new HashMap<>();
+        beans.put("app", app);
+        XLSReadStatus readStatus = reader.read(inputXLS, beans);
         return false;
     }
 }
--- src/main/java/de/codecentric/springbootsample/fetcher/IFetcher.java
+++ src/main/java/de/codecentric/springbootsample/fetcher/IFetcher.java
@@ -1,5 +1,10 @@
 package de.codecentric.springbootsample.fetcher;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
 public interface IFetcher {
-    public boolean fetchExcel();
+    public boolean fetchExcel() throws IOException, SAXException, InvalidFormatException;
 }
